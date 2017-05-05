1c5720341bb9e3ad0e2db5af633af64e5c201acd
Paul Shealy <paulsh@microsoft.com>
2017-05-05 11:09:50 -0700

    Added CHAINER files for nvidia-docker demo

--- Extensions/General/create-user-and-updatecntk.sh
+++ Extensions/General/create-user-and-updatecntk.sh
@@ -15,6 +15,12 @@ pip install --upgrade --no-deps https://cntk.ai/PythonWheel/GPU/cntk-2.0rc2-cp35
 cd /etc/skel/notebooks
 python -m cntk.sample_installer
 
+mkdir /etc/skel/CHAINER
+cd /etc/skel/CHAINER
+wget https://raw.githubusercontent.com/Microsoft/AI-Immersion-Workshop/master/Deep%20Learning%20and%20the%20Microsoft%20Cognitive%20Toolkit/CNTK%20Hands-on%20in%20GPU%20DSVMs/download_MNIST_chainer.py
+python download_MNIST_chainer.py
+wget https://raw.githubusercontent.com/Microsoft/AI-Immersion-Workshop/master/Deep%20Learning%20and%20the%20Microsoft%20Cognitive%20Toolkit/CNTK%20Hands-on%20in%20GPU%20DSVMs/MNIST_chainer.py
+
 for i in $(seq 1 $num);  do
   u=`openssl rand -hex 2`;
   p=`openssl rand -hex 3`;
