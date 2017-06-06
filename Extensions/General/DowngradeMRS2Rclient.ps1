40c577d2c16a1f482ae64f648107bbcf85d42490
Gopi Kumar <gopitk@users.noreply.github.com>
2017-06-06 00:53:38 -0700

    Install R packages, Jupyter kernel

--- Extensions/General/DowngradeMRS2Rclient.ps1
+++ Extensions/General/DowngradeMRS2Rclient.ps1
@@ -11,5 +11,14 @@ cmd /c "c:\tmp\RClientSetup.exe /quiet"
 set-Service -name "MSSQLLaunchpad" -StartupType Disabled
 Stop-Service -name "MSSQLLaunchpad"
 
+# Set Machine path with R client
+$path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + "C:\Program Files\Microsoft\R Client\R_SERVER\bin\x64"
+[environment]::SetEnvironmentVariable("path", "$path","machine")
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+# Install some R libraries and Jupyter R Kernel. You can add  other libraries you want to install to the list below.
+cmd /c rscript.exe -e "install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest', 'dplyr', 'ggplot2', 'AzureML', 'xgboost', 'stringi', 'stringr', 'tidyr'))"
+cmd /c rscript.exe -e "devtools::install_github('IRkernel/IRkernel')"
+
 # Cleanup
 del C:\tmp\RClientSetup.exe
