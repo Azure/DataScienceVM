33f444204e7c3f9947548008a4f6a2f2c2067f5a
Paul Shealy <paulsh@microsoft.com>
2018-01-08 16:12:11 -0800

    Specify the conda environment to use

--- Extensions/IoTEdge/install.sh
+++ Extensions/IoTEdge/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install IOT Edge Runtime and register the edge device
-source /anaconda/bin/activate
-/anaconda/bin/pip install -U azure-iot-edge-runtime-ctl
-/anaconda/bin/iotedgectl setup --connection-string $1 --auto-cert-gen-force-no-passwords
-/anaconda/bin/iotedgectl start
+source /anaconda/bin/activate root
+pip install -U azure-iot-edge-runtime-ctl
+iotedgectl setup --connection-string $1 --auto-cert-gen-force-no-passwords
+iotedgectl start
