fb40b90059b2641dd7c62c4adfc1dd590894f736
Paul Shealy <paulsh@microsoft.com>
2018-01-09 13:35:58 -0800

    Add the user to Docker and change the config file permissions

--- Extensions/IoTEdge/install.sh
+++ Extensions/IoTEdge/install.sh
@@ -4,7 +4,11 @@
 # HOME is not set when the extension runs, so set it now
 export HOME=$2
 
+usermod -aG docker $2
+
 source /anaconda/bin/activate root
 pip install -U azure-iot-edge-runtime-ctl
 iotedgectl setup --connection-string $1 --auto-cert-gen-force-no-passwords
 iotedgectl start
+
+chmod 644 /etc/azure-iot-edge/config.json
