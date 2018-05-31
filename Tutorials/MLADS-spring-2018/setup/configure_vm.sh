999ce3146e0bf4f1cadc48c3e8dbe5192dc2e7aa
Paul Shealy <paulsh@microsoft.com>
2018-05-31 00:37:13 +0000

    Don't need to download the dataset twice

--- Tutorials/MLADS-spring-2018/setup/configure_vm.sh
+++ Tutorials/MLADS-spring-2018/setup/configure_vm.sh
@@ -11,9 +11,6 @@ echo "spark.executor.cores 1" >> /dsvm/tools/spark/current/conf/spark-defaults.c
 cd /opt/microsoft/mlserver/9.2.1/o16n
 sudo dotnet Microsoft.MLServer.Utils.AdminUtil/Microsoft.MLServer.Utils.AdminUtil.dll -silentoneboxinstall Dsvm@123
 
-# download the CIFAR-10 dataset
-wget -O /data/cifar-10-python.tar.gz http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-
 # download the notebooks
 mkdir /etc/skel/notebooks/MLADS-spring-2018
 git clone https://github.com/Azure/DataScienceVM /data/DataScienceVM
