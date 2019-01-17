0aad7d33876f4dd80bbc9ed6867e58b7aeccc1fd
Gopi Kumar <gopitk@users.noreply.github.com>
2019-01-16 18:38:28 -0800

    Update notebook metadata

--- Extensions/fastaiv1/fastaiLinux.sh
+++ Extensions/fastaiv1/fastaiLinux.sh
@@ -5,9 +5,32 @@ source /anaconda/bin/activate fastai
 /anaconda/bin/conda install  -y -c fastai fastai
 /anaconda/bin/conda install  -y ipykernel
 python -m ipykernel install --name 'fastai' --display-name 'Python (fastai)'
+# Script to update Notbook metadata
+cat << EOF > /tmp/changenbmeta.py
+#!/usr/bin/python
+import json,sys
+
+if len(sys.argv) < 2 :
+        print "Usage: python changenbmeta.py <filename>"
+        exit()
+with open(sys.argv[1], "r") as jsonFile:
+    data = json.load(jsonFile)
+
+data["metadata"]["kernelspec"]["display_name"] = "Python (fastai)"
+data["metadata"]["kernelspec"]["name"] = "fastai"
+
+with open(sys.argv[1], "w") as jsonFile:
+    json.dump(data, jsonFile)
+
+EOF
+
 mkdir -p /etc/skel/notebooks/fastai
 cd /etc/skel/notebooks/fastai
 git clone https://github.com/fastai/course-v3
+
+# Change metadata on notebook to match kernel name in the fast.ai notebooks
+find . -name \*.ipynb -exec /usr/bin/python /tmp/changenbmeta.py {} \;
+
 # copy the notebooks to the initial user's profile
 for filename in /home/*; do
   dir=$filename/notebooks
