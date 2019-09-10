42486d82a1de5ced6ea17a1e7cc0956495e02880
Gopi Kumar <gopitk@users.noreply.github.com>
2019-09-10 16:17:54 -0700

    Update configure_vm.sh

--- Tutorials/MLADS-fall-2018/setup/configure_vm.sh
+++ Tutorials/MLADS-fall-2018/setup/configure_vm.sh
@@ -3,13 +3,6 @@
 source /etc/profile
 conda activate py36
 
-# enable the widgets 
-wget https://dsvmassets.blob.core.windows.net/mladsfall2018/azureml_train_widgets.tar.gz -O /tmp/azureml_train_widgets.tar.gz
-tar xzf /tmp/azureml_train_widgets.tar.gz -C /usr/local/share/jupyter/nbextensions
-
-mkdir -p /etc/skel/.jupyter/nbconfig
-echo '{ "load_extensions": { "azureml_train_widgets/extension": true } }' | tee /etc/skel/.jupyter/nbconfig/notebook.json
-
 # install h5py for Keras
 conda install h5py -y
 
@@ -27,25 +20,25 @@ for filename in /home/*; do
   chown $user $dir/MLADS-fall-2018
 done
 
-# host=`hostname`
-# # create users
-# # we are skipping this part now that MLADS is over, and most people using this template want
-# # to use it with the initial user account
-# for i in $(seq 1 6);  do
-#   u=`openssl rand -hex 2`;
-#   # replace 1 with g
-#   u=`echo $u | sed -e 's/1/g/g'`
-#   # replace 0 with h
-#   u=`echo $u | sed -e 's/0/h/g'`
+host=`hostname`
+# create users
+# we are skipping this part now that MLADS is over, and most people using this template want
+# to use it with the initial user account
+for i in $(seq 1 6);  do
+  u=`openssl rand -hex 2`;
+  # replace 1 with g
+  u=`echo $u | sed -e 's/1/g/g'`
+  # replace 0 with h
+  u=`echo $u | sed -e 's/0/h/g'`
 
-#   p=`openssl rand -hex 4`;
-#   # replace 1 with g
-#   p=`echo $p | sed -e 's/1/g/g'`
-#   # replace 0 with h
-#   p=`echo $p | sed -e 's/0/h/g'`
+   p=`openssl rand -hex 4`;
+  # replace 1 with g
+  p=`echo $p | sed -e 's/1/g/g'`
+  # replace 0 with h
+  p=`echo $p | sed -e 's/0/h/g'`
 
-#   useradd -m -d /home/user$u -s /bin/bash user$u
-#   echo user$u:$p | chpasswd
-#   echo $host, user$u, $p >> '/data/usersinfo.csv';
-#   usermod -aG docker user$u
-# done
+  useradd -m -d /home/user$u -s /bin/bash user$u
+  echo user$u:$p | chpasswd
+  echo $host, user$u, $p >> '/data/usersinfo.csv';
+  usermod -aG docker user$u
+done
