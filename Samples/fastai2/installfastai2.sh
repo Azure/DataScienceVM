dcf1e9db11cab5b7301bbfe480c7bbeaef95a21d
zenlytix <gopitk@users.noreply.github.com>
2020-08-29 00:10:15 -0700

    Need a fastbook lib to run course

--- Samples/fastai2/installfastai2.sh
+++ Samples/fastai2/installfastai2.sh
@@ -7,4 +7,5 @@ git clone https://github.com/fastai/course-v4.git
 conda create -y -n fastai2 python=3.7
 conda activate fastai2
 pip install -r fastbook/requirements.txt
+pip install -Uqq fastbook
 EOF
