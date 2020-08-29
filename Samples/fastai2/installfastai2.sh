543c312bb0599b0f9b7f6eb8fd90215ca3778566
zenlytix <gopitk@users.noreply.github.com>
2020-08-28 23:45:52 -0700

    Changes related to public course v4 launch

--- Samples/fastai2/installfastai2.sh
+++ Samples/fastai2/installfastai2.sh
@@ -4,11 +4,7 @@ sudo -i -u fastuser <<EOF
 cd notebooks/
 git clone https://github.com/fastai/fastbook.git
 git clone https://github.com/fastai/course-v4.git
-cd ..
-git clone https://github.com/fastai/fastai2.git
-cd fastai2
-cat environment.yml
-conda env create -f environment.yml
+conda create -y -n fastai2 python=3.7
 conda activate fastai2
-pip install -r ../notebooks/fastbook/requirements.txt
+pip install -r fastbook/requirements.txt
 EOF
